58b3373af0a69f4647d510b65c6ed6635415da4f
Matthew McGrath <rubywep@hotmail.com>
2022-05-23 14:14:49 -0700

    Add mock data for Post objects

---
+++ mockPosts.js
@@ -0,0 +1,39 @@
+const MOCK_POSTS = [
+  {
+    date: '05-11-2022',
+    title: 'Some Title',
+    slug: 'slug1',
+    excerpt: 'This is some paragraph',
+    image: 'post1.jpg',
+  },
+  {
+    date: '05-13-2022',
+    title: 'Some Title',
+    slug: 'slug2',
+    excerpt: 'This is some paragraph',
+    image: 'post2.jpg',
+  },
+  {
+    date: '05-16-2022',
+    title: 'Some Title',
+    slug: 'slug3',
+    excerpt: 'This is some paragraph',
+    image: 'post3.jpg',
+  },
+  {
+    date: '05-18-2022',
+    title: 'Some Title',
+    slug: 'slug1',
+    excerpt: 'This is some paragraph',
+    image: 'post1.jpg',
+  },
+  {
+    date: '05-23-2022',
+    title: 'Some Title',
+    slug: 'slug2',
+    excerpt: 'This is some paragraph',
+    image: 'post2.jpg',
+  },
+];
+
+export default MOCK_POSTS;
