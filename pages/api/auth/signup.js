187cccf3e41bf5c39bd9c721e3687d749a8e4ba9
Matthew McGrath <rubywep@hotmail.com>
2022-05-30 18:47:10 -0700

    Begin route handler for signup auth

---
+++ pages/api/auth/signup.js
@@ -0,0 +1,30 @@
+import { connectToDatabase } from '../../../utils/databaseUtilities';
+import { hashPassword } from '../../../utils/authUtilities';
+
+async function handler(req, res) {
+  if (req.method !== 'post') {
+    return;
+  }
+
+  const { data } = req.body;
+  const { email, password } = data;
+
+  if (!email || !email.includes('@') || !password || password.trim().length < 7) {
+    return res.status(422).json({ message: 'Invalid user input' });
+  }
+
+  const client = await connectToDatabase();
+  const db = client.db();
+
+  const hashedPassword = await hashPassword(password);
+
+  // Create and store new User
+  const result = await db.collection('users').insertOne({
+    email,
+    password: hashedPassword,
+  });
+
+  res.status(201).json({ message: 'User created successfully!' });
+}
+
+export default handler;
